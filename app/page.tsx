1ccf89d68e42d5b7f06e5a363a4da2294819c061
jslikai918 <jslikai918@gmail.com>
2024-09-21 17:10:45 +0800

    fix: warning

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 import WebApp from "@twa-dev/sdk";
-import { useEffect, useState } from "react";
-import { Fernet } from 'fernet';
+import { useEffect, useState, useRef } from "react";
+import { message } from 'antd';
+import generateInviteCode from './utils/encode_decode';
 
-// Define a base URL for your API
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://139.177.202.65:6543';
-
+const API_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://139.177.202.65:6543' : '/api';
 interface UserRegistration {
   id: number;
   registrationDate: string;  // format: "YYYY.MM"
@@ -23,27 +22,15 @@ const userRegistrations: UserRegistration[] = [
   { id: 6245952118, registrationDate: "2023.07" }
 ];
 
-// Add these functions outside of the Home component
-function encryptString(text: string, key: string): string {
-  const fernet = new Fernet(key);
-  return fernet.encode(text);
-}
-
-function decryptString(encryptedText: string, key: string): string {
-  const fernet = new Fernet(key);
-  return fernet.decode(encryptedText);
-}
-
 export default function Home() {
-  const [recommendedUserNumber, setRecommendedUserNumber] = useState(0);
   const [tokenToTake, setTokenToTake] = useState(0);
   const [btcToTake, setBtcToTake] = useState(0);
   const [userId, setUserId] = useState<string | null>(null);
-  const [years, setYears] = useState(1);
-  const [encryptionKey, setEncryptionKey] = useState<string | null>(null);
-  // const [registerYears, setRegisterYears] = useState(0);
-  // const [recommender, setRecommender] = useState<string>("0");
-
+  const [registerYears, setRegisterYears] = useState(0);
+  const [recommender, setRecommender] = useState<string>("0");
+  const dataFetchedRef = useRef(false);
+  const [messageApi, contextHolder] = message.useMessage();
+  console.log('process.env', process)
   useEffect(() => {
     // Initialize WebApp and get user_id
     WebApp.ready();
@@ -51,28 +38,30 @@ export default function Home() {
     const user = WebApp.initDataUnsafe.user;
     if (user) {
       setUserId(user.id.toString());
-      // setRegisterYears(binarySearch(user.id));
+      setRegisterYears(binarySearch(user.id));
     }
 
     if (WebApp.initDataUnsafe.start_param) {
       // post ref request to server
-      // setRecommender(WebApp.initDataUnsafe.start_param);
+      setRecommender(WebApp.initDataUnsafe.start_param);
     }
 
     // Generate or retrieve an encryption key
-    const key = process.env.NEXT_PUBLIC_ENCRYPTION_KEY!
-    setEncryptionKey(key);
+    console.log('process.env', process)
   }, []);
 
   useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+
     const fetchData = async () => {
       setUserId('1390026482')
-      setYears(1)
-      if (!userId) return;
+      setRegisterYears(1)
+      // if (!userId) return;
 
       try {
         // Use the API_BASE_URL and interpolate the userId
-        const recommendationResponse = await fetch(`${API_BASE_URL}/api/user/score?userId=${userId}&years=${years}`);
+        const recommendationResponse = await fetch(`${API_BASE_URL}/api/user/score?user_id=${userId}&years=${registerYears}`);
         // const recommendationResponse = await fetch('/api', { method: 'GET'});
         console.log(recommendationResponse)
         if (!recommendationResponse.ok) {
@@ -82,45 +71,40 @@ export default function Home() {
         console.log('API Response:', response);
         
         // Uncomment these lines when the API is ready
-        setRecommendedUserNumber(response.recommendations);
         setTokenToTake(response.token_score);
-        setBtcToTake(response.btc_score);
-        
-        // Remove these mock values when the API is ready
-        // setRecommendedUserNumber(23);
-        // setTokenToTake(100);
-        // setBtcToTake(50);
+        const btcScore = Number((response.btc_score * 100).toFixed(6));
+        setBtcToTake(btcScore);
       } catch (error) {
         console.error('Error fetching data:', error);
-        // Optionally, set an error state here to display to the user
       }
 
       // Set recommand
-      // try {
-      //   const encryptedUserId = encryptString(userId, process.env.NEXT_PUBLIC_ENCRYPTION_KEY!);
-      //   const encryptedOldUser = encryptString(recommender, process.env.NEXT_PUBLIC_ENCRYPTION_KEY!);
-      //   const refResponse = await fetch(`${API_BASE_URL}/api/recommend?new_user=${encryptedUserId}&old_user=${encryptedOldUser}`);
-      //   if (!refResponse.ok) {
-      //     throw new Error('API request failed');
-      //   }
-      //   const response = await refResponse.json();
-      //   console.log('API Response:', response);
-      // } catch (error) {
-      //   console.error('Error fetching data:', error);
-      // }
+      try {
+        const encryptedOldUser = generateInviteCode(Number(userId));
+        const refResponse = await fetch(`${API_BASE_URL}/api/recommend`, { method: 'POST',headers: {
+          'Content-Type': 'application/json',
+        }, body: JSON.stringify({"old_user": encryptedOldUser, "new_user": recommender})});
+        if (!refResponse.ok) {
+          throw new Error('API request failed');
+        }
+        const response = await refResponse.json();
+        console.log('API Response:', response);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
 
     fetchData();
-  }, [userId]);
+  }, []);
 
   function calculateYearsSince(dateString: string) {
     const now = new Date();
     const [year, month] = dateString.split('.').map(Number);
     const registrationDate = new Date(year, month - 1);
     const diff = now.getTime() - registrationDate.getTime();
-    return diff / (1000 * 60 * 60 * 24 * 365);  // returns decimal years
+    return diff / (1000 * 60 * 60 * 24 * 365);  // returns decimal registerYears
   }
-  
+
   function binarySearch(userId: number) {
     let low = 0;
     let high = userRegistrations.length - 1;
@@ -141,15 +125,38 @@ export default function Home() {
     return 1;  
   }
 
+  async function handleInvite() {
+    
+    console.log('handleInvite')
+    // https://t.me/ppppooogg_bot/pdd123?startapp=1BSNLB
+    const recommendationResponse = await fetch(`${API_BASE_URL}/api/user/referral-code/${userId}`)
+    if (!recommendationResponse.ok) {
+      throw new Error('API request failed');
+    }
+    const response = await recommendationResponse.json();
+    console.log(response)
+    await navigator.clipboard.writeText(`https://t.me/ppppooogg_bot/pdd123?startapp=${response.referral_code}`)
+    messageApi.open({
+      type: 'success',
+      content: 'Copy Success!',
+    });
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">PDD Referral</h1>
-      
-      <div className="space-y-4">
-        <p><strong>Recommended User Number:</strong> {recommendedUserNumber}</p>
-        <p><strong>Token to be taken:</strong> {tokenToTake}</p>
-        <p><strong>BTC to be taken:</strong> {(btcToTake / 100).toFixed(2)}%</p>
+    <div className="bg-black h-screen px-16 py-10">
+      {contextHolder}
+      <img className='w-52 h-44 mx-auto' src="/images/final.svg" alt="" />
+      <h3 className='mt-8 text-white text-center'>Referral Reward</h3>
+      <div className='w-full border border-teal-600 rounded p-1'>
+        <div className='p-2 bg-lime-300 rounded-sm relative'>
+          <div className='w-20 bg-lime-500 h-4 rounded-e-lg'  style={{width: btcToTake + '%'}}></div>
+          <span className='absolute top-1 inset-x-1/2 translate-x-negative-5 text-black translate-x-50'>{btcToTake}%</span>
+        </div>
       </div>
-    </main>
+      <p className="text-white text-center mt-12">Token Reward</p>
+      <p className="text-white text-center mt-10"><span className="text-lime-600">{tokenToTake}</span>$AAA</p>
+
+      <div className="bg-lime-500 text-black text-center mt-16 h-10 leading-10 rounded-lg" onClick={handleInvite}>Invite Friends</div>
+    </div>
   );
 }
