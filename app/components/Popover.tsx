78638cc2c196728193cd9405ef65080f2240341a
yijie37 <yijiehere@163.com>
2024-09-28 09:20:40 +0800

    reset progress

--- app/components/Popover.tsx
+++ app/components/Popover.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Popover } from 'antd';
 
 interface IPopoverProps {
@@ -11,6 +11,22 @@ interface IPopoverProps {
 }
 
 const App: React.FC<IPopoverProps> = ({ headImg, firstLine, secondLine, hideHeaderImg=true }) => {
+  // const [visible, setVisible] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleOpenChange = (newOpen: boolean) => {
+    setOpen(newOpen);
+  };
+
+
+  // const handleVisibleChange = (newVisible: boolean) => {
+  //   setVisible(newVisible);
+  // };
+
+  // const handleClick = () => {
+  //   setVisible(!visible);
+  // };
+
   const content = (
     <div className='text-white'>
       {hideHeaderImg && <img className='w-12 mx-auto' src={`/images/${headImg}`} alt="Head Image" />}
@@ -19,9 +35,23 @@ const App: React.FC<IPopoverProps> = ({ headImg, firstLine, secondLine, hideHead
     </div>
   );
 
+  // return (
+  //   <Popover content={content}>
+  //     <img className="w-6 pos-right" src="/images/Infomation.svg" alt="Information Icon" />
+  //   </Popover>
+  // );
   return (
-    <Popover content={content}>
-      <img className="w-6 pos-right" src="/images/Infomation.svg" alt="Information Icon" />
+    <Popover 
+      content={content}
+      open={open}
+      onOpenChange={handleOpenChange}
+      trigger="click"
+    >
+      <img 
+        className="w-6 pos-right cursor-pointer" 
+        src="/images/Infomation.svg" 
+        alt="Information Icon" 
+      />
     </Popover>
   );
 };
