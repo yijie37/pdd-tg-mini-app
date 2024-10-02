a25be1f85a26b1f4b8383a8c72fa3131bdf019db
bcmaster2049 <sql23mz@gmail.com>
2024-10-03 06:55:55 +0800

    update infection number

--- app/page.tsx
+++ app/page.tsx
@@ -274,7 +274,7 @@ export default function Home() {
       
       <div className="mt-4 sm:mt-6 space-y-2 sm:space-y-4">
         {/* {Number(recommendations) > 0 && ( */}
-          <div className="text-white-500 mt-1 text-xs">You Have Infected {recommendations} people</div>
+        <p className="text-white text-center mb-4">You Have Infected {recommendations} people</p>
         {/* )} */}
         <div className="bg-lime-500 text-black text-center h-10 leading-10 rounded-lg" onClick={handleInvite}>
           Infect Others
